e3cebfd173f636c73373ea56e96d44924ee05baf
John Ravan A. Dizon <johnravand@gmail.com>
2023-05-28 21:12:21 +0800

    initial commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+const input1 = document.querySelector("input1");
+const input2 = document.querySelector("input2");
+const btn1 = document.querySelector("#btn1");
+
+btn1.addEventListener("click", ()=>{
+    console.log('It was clicked!')
+})
